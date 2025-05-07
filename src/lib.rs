2d0f51f743d750b482782dfd85c549e3d1c73ec0
Jady Wright <iamjwright159@gmail.com>
2025-05-07 19:15:39 -0400

    space formatting

--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,26 @@ pub use midi::MidiTrack;
 pub use notes::Note;
 pub use rustysynth::SoundFont;
 pub use source::{
-	MidiAudio, MidiAudioTrack, MidiAudioTrackHandle, MidiBufferMessage, MidiQueueEvent,
-	MidiQueueEventType, MidiQueueLooping, MidiQueueTiming, SyncedMidiInfo,
+    MidiAudio, MidiAudioTrack, MidiAudioTrackHandle, MidiBufferMessage, MidiQueueEvent,
+    MidiQueueEventType, MidiQueueLooping, MidiQueueTiming, SyncedMidiInfo,
 };
 
 mod midi;
 mod notes;
 mod source;
 
+#[derive(Default)]
 pub struct SoundyPlugin;
 
 impl Plugin for SoundyPlugin {
-	fn build(&self, app: &mut App) {
-		app.add_audio_source::<MidiAudio>()
-			.add_systems(PreUpdate, tick_sequencers);
-	}
+    fn build(&self, app: &mut App) {
+        app.add_audio_source::<MidiAudio>()
+            .add_systems(PreUpdate, tick_sequencers);
+    }
 }
 
 fn tick_sequencers(mut audios: ResMut<Assets<MidiAudio>>, time: Res<Time>) {
-	for (_id, audio) in audios.iter_mut() {
-		audio.tick(time.delta());
-	}
+    for (_id, audio) in audios.iter_mut() {
+        audio.tick(time.delta());
+    }
 }
--- src/midi.rs
+++ src/midi.rs
@@ -2,108 +2,108 @@ use std::borrow::Borrow;
 use std::ops::Index;
 
 use augmented_midi::{
-	parse_midi_file, MIDIFile, MIDIFileChunk, MIDIFileDivision, MIDIMessage, MIDIMessageNote,
-	MIDITrackInner,
+    MIDIFile, MIDIFileChunk, MIDIFileDivision, MIDIMessage, MIDIMessageNote, MIDITrackInner,
+    parse_midi_file,
 };
 use itertools::Itertools;
 
 #[derive(Debug, Clone)]
 pub struct MidiTrackAccumulateEvent {
-	pub time: u64,
-	pub inner: MidiEvent,
+    pub time: u64,
+    pub inner: MidiEvent,
 }
 
 #[derive(Debug, Clone)]
 pub struct MidiTrack {
-	pub events: Vec<MidiTrackAccumulateEvent>,
-	pub ticks_per_beat: u16,
+    pub events: Vec<MidiTrackAccumulateEvent>,
+    pub ticks_per_beat: u16,
 }
 
 impl MidiTrack {
-	pub fn from_midi_file<
-		StringRepr: Borrow<str>,
-		Buffer: Borrow<[u8]> + Clone + Index<usize, Output = u8>,
-	>(
-		file: MIDIFile<StringRepr, Buffer>,
-	) -> Self {
-		let events = file
-			.chunks
-			.iter()
-			.filter_map(|chunk| match chunk {
-				MIDIFileChunk::Track { events } => Some(events.clone()),
-				_ => None,
-			})
-			.enumerate()
-			.flat_map(|(i, track)| {
-				let mut time = 0;
-				track
-					.iter()
-					.filter_map(|event| {
-						time += event.delta_time as u64;
-						let inner = match &event.inner {
-							MIDITrackInner::Message(MIDIMessage::NoteOn(MIDIMessageNote {
-								channel,
-								note,
-								velocity,
-							})) => MidiEvent::NoteOn {
-								channel: (*channel).max(i as u8), // Workaround for DAWs that don't set the channel
-								note: *note,
-								velocity: *velocity,
-							},
-							MIDITrackInner::Message(MIDIMessage::NoteOff(MIDIMessageNote {
-								channel,
-								note,
-								velocity: _,
-							})) => MidiEvent::NoteOff {
-								channel: (*channel).max(i as u8),
-								note: *note,
-							},
-							MIDITrackInner::Meta(meta) if meta.meta_type == 0x51 => {
-								let microseconds_per_beat = u32::from_be_bytes([
-									0,
-									meta.bytes[0],
-									meta.bytes[1],
-									meta.bytes[2],
-								]);
-								let tempo = 60_000_000.0 / microseconds_per_beat as f64;
-								MidiEvent::SetTempo { tempo }
-							}
-							_ => return None,
-						};
-						Some(MidiTrackAccumulateEvent { time, inner })
-					})
-					.collect::<Vec<_>>()
-			})
-			.sorted_by_key(|event| event.time)
-			.collect::<Vec<_>>();
+    pub fn from_midi_file<
+        StringRepr: Borrow<str>,
+        Buffer: Borrow<[u8]> + Clone + Index<usize, Output = u8>,
+    >(
+        file: MIDIFile<StringRepr, Buffer>,
+    ) -> Self {
+        let events = file
+            .chunks
+            .iter()
+            .filter_map(|chunk| match chunk {
+                MIDIFileChunk::Track { events } => Some(events.clone()),
+                _ => None,
+            })
+            .enumerate()
+            .flat_map(|(i, track)| {
+                let mut time = 0;
+                track
+                    .iter()
+                    .filter_map(|event| {
+                        time += event.delta_time as u64;
+                        let inner = match &event.inner {
+                            MIDITrackInner::Message(MIDIMessage::NoteOn(MIDIMessageNote {
+                                channel,
+                                note,
+                                velocity,
+                            })) => MidiEvent::NoteOn {
+                                channel: (*channel).max(i as u8), // Workaround for DAWs that don't set the channel
+                                note: *note,
+                                velocity: *velocity,
+                            },
+                            MIDITrackInner::Message(MIDIMessage::NoteOff(MIDIMessageNote {
+                                channel,
+                                note,
+                                velocity: _,
+                            })) => MidiEvent::NoteOff {
+                                channel: (*channel).max(i as u8),
+                                note: *note,
+                            },
+                            MIDITrackInner::Meta(meta) if meta.meta_type == 0x51 => {
+                                let microseconds_per_beat = u32::from_be_bytes([
+                                    0,
+                                    meta.bytes[0],
+                                    meta.bytes[1],
+                                    meta.bytes[2],
+                                ]);
+                                let tempo = 60_000_000.0 / microseconds_per_beat as f64;
+                                MidiEvent::SetTempo { tempo }
+                            }
+                            _ => return None,
+                        };
+                        Some(MidiTrackAccumulateEvent { time, inner })
+                    })
+                    .collect::<Vec<_>>()
+            })
+            .sorted_by_key(|event| event.time)
+            .collect::<Vec<_>>();
 
-		Self {
-			events,
-			ticks_per_beat: match file
-				.header()
-				.expect("MIDI file must have a header chunk")
-				.division
-			{
-				MIDIFileDivision::TicksPerQuarterNote {
-					ticks_per_quarter_note,
-				} => ticks_per_quarter_note,
-				_ => panic!("Invalid MIDI file division"),
-			},
-		}
-	}
+        Self {
+            events,
+            ticks_per_beat: match file
+                .header()
+                .expect("MIDI file must have a header chunk")
+                .division
+            {
+                MIDIFileDivision::TicksPerQuarterNote {
+                    ticks_per_quarter_note,
+                } => ticks_per_quarter_note,
+                _ => panic!("Invalid MIDI file division"),
+            },
+        }
+    }
 
-	pub fn from_bytes(bytes: &[u8]) -> Self {
-		Self::from_midi_file(
-			parse_midi_file::<String, Vec<u8>>(bytes)
-				.expect("Failed to parse MIDI file")
-				.1,
-		)
-	}
+    pub fn from_bytes(bytes: &[u8]) -> Self {
+        Self::from_midi_file(
+            parse_midi_file::<String, Vec<u8>>(bytes)
+                .expect("Failed to parse MIDI file")
+                .1,
+        )
+    }
 }
 
 #[derive(Debug, Clone)]
 pub enum MidiEvent {
-	NoteOn { channel: u8, note: u8, velocity: u8 },
-	NoteOff { channel: u8, note: u8 },
-	SetTempo { tempo: f64 },
+    NoteOn { channel: u8, note: u8, velocity: u8 },
+    NoteOff { channel: u8, note: u8 },
+    SetTempo { tempo: f64 },
 }
